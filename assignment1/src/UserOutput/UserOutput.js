c3cb93e98fc70edff8365a533773d8317248b33e
marcelfranca <Nan44aocw@#>
2020-01-12 23:37:57 -0300

    render xist

---
+++ assignment1/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Some Random text</p>
+            <p>Some Random text</p>
+        </div>
+    )
+};
+
+export default userOutput();
